edbdbcf0318e9e64380f6fefb56d49310abf66d5
viiiga <126700032+viiiga@users.noreply.github.com>
2023-09-28 16:38:06 -0300

    Update principal.py
    Pequena alteração no input inicial do jogo, além da remoção de recados.

--- jogo_da_velha/principal.py
+++ jogo_da_velha/principal.py
@@ -8,16 +8,13 @@ from funcoes import printar_placar
 placar = [0, 0]  # Inicialização do placar (X: placar[0], O: placar[1])
 
 while True:
-    # Inicialização do tabuleiro
     tabuleiro = ['_' for _ in range(9)]
 
-    # Variáveis do jogo
     jogador = 1
 
     while True:
-        # Verifica de quem é a vez
         simbolo = 'X' if jogador == 1 else 'O'
-        jogador_input = input(f'Jogador {jogador} Digite uma posição de 1 a 9: ')
+        jogador_input = input(f'Jogador {jogador} Digite uma posição de 1 a 9 (sendo 1 -> 1x1 e 9 -> 3x3): ')
 
         try:
             posicao = int(jogador_input) - 1
@@ -28,15 +25,15 @@ while True:
 
                 if verifica_vitoria(tabuleiro, simbolo):
                     print(f'Jogador {jogador} ganhou!')
-                    placar[jogador - 1] += 1  # Atualiza o placar
-                    salvar_placar(jogador, placar)  # Salva o resultado da partida no arquivo
+                    placar[jogador - 1] += 1 
+                    salvar_placar(jogador, placar) 
                     break
                 elif verificar_empate(tabuleiro):
                     print('Empate!')
-                    salvar_placar(0, placar)  # Salva o resultado da partida no arquivo (empate)
+                    salvar_placar(0, placar) 
                     break
                 else:
-                    jogador = 3 - jogador  # Alternar entre jogador 1 e 2
+                    jogador = 3 - jogador  
             else:
                 print(desenha_tabuleiro(tabuleiro))
                 print('Posicao invalida ou ocupada.')
